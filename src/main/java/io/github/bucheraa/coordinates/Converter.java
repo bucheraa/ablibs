5461950fbce62eb71d0586e47ed896cb1ba472bd
bucheraa <33252573+bucheraa@users.noreply.github.com>
2021-08-10 16:34:43 +0200

    Added method descriptions

--- src/main/java/io/github/bucheraa/coordinates/Converter.java
+++ src/main/java/io/github/bucheraa/coordinates/Converter.java
@@ -1,29 +1,55 @@
 package io.github.bucheraa.coordinates;
 
+import org.jetbrains.annotations.NotNull;
+
 import java.text.DecimalFormat;
 
 public class Converter {
 
-    @org.jetbrains.annotations.NotNull
+    /**
+     * converts decimal coordinates (latitude, longitude) into Degrees° Minutes' Seconds"
+     * @param latitude
+     * @param longitude
+     * @return
+     */
     public static String decimal2DMS(final double latitude, final double longitude) {
         return getStringDMS(latitude) + " " + getStringDMS(longitude);
     }
 
-    @org.jetbrains.annotations.NotNull
+    /**
+     * converts decimal latitude OR longitude into Degrees° Minutes' Seconds"
+     * @param latLon
+     * @return
+     */
     private static String getStringDMS(final double latLon) {
         return getDegree(latLon) + "° " + getMinute(latLon) + "' " + new DecimalFormat("##.##").format(getSecond(latLon)) + "\"";
     }
 
+    /**
+     * returns number before decimal point
+     * @param latLon
+     * @return
+     */
     private static int getDegree(final double latLon) {
         return (int) latLon;
     }
 
+    /**
+     * returns minutes from given dec coordinate
+     * @param latLon
+     * @return
+     */
     private static int getMinute(final double latLon) {
         double numberAfterPoint = latLon - Math.floor(latLon);
         double minuteTemp = numberAfterPoint * 60;
         return (int) minuteTemp;
     }
 
+    /**
+     * returns seconds from given dec coordinate
+     * @param latLon
+     * @return
+     */
     private static double getSecond(final double latLon) {
         double numberAfterPoint = latLon - Math.floor(latLon);
         double minuteTemp = numberAfterPoint * 60;
